b33da51fd134b6973970164d902878c718414390
Nikshith <145009603+alexnikshith@users.noreply.github.com>
2026-01-08 19:13:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -234,6 +234,7 @@ import sqlite3
 from utils.qr_generator import generate_qr
 from datetime import date, datetime
 import json
+import os
 
 app = Flask(__name__)
 app.secret_key = "smart_attendance_secret"
@@ -496,5 +497,8 @@ def logout():
 # =========================
 if __name__ == "__main__":
     # app.run(host="127.0.0.1", port=5000, debug=False)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
